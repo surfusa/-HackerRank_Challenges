fa773edc4e10a3fd984b2a3de4d413f38737a6e2
James Phillips <surfusa@users.noreply.github.com>
2021-01-26 22:26:53 -0700

    Upload 2D Arrays Solution
    Upload of 2D Arrays DS method and the visible unit test of the method from HackerRank

---
+++ HackerRank/Arrays/TwoDArrayDS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class TwoDArrayDS
+    {
+        public TwoDArrayDS()
+        {
+
+        }
+
+        public int GetLargestHourGlassValue(int[][] arr)
+        {
+            int size = arr.Length;
+            int hourglassSum = 0, localHourglass;
+
+            for (int i = 1; i < (size - 1); i++)
+            {
+                for (int j = 1; j < (size - 1); j++)
+                {
+                    localHourglass = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i - 1][j + 1] + arr[i][j] + arr[i + 1][j - 1] + arr[i + 1][j] + arr[i + 1][j + 1];
+                    if (i == 1 && j == 1)
+                    {
+                        hourglassSum = localHourglass;
+                    }
+                    if (localHourglass > hourglassSum)
+                    {
+                        hourglassSum = localHourglass;
+                    }
+                }
+            }
+            return hourglassSum;
+        }
+    }
+}
---
+++ HackerRank/ArraysTests/TwoDArrayDSTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays.Tests
+{
+    [TestClass()]
+    public class TwoDArrayDSTests
+    {
+        [TestMethod()]
+        public void GetLargestHourGlassValueTest_Success1()
+        {
+            // Arrange
+            var expectedSuccessfulTest = 19;
+            var successfulTestStr = "1 1 1 0 0 0\n0 1 0 0 0 0\n1 1 1 0 0 0\n0 0 2 4 4 0\n0 0 0 2 0 0\n0 0 1 2 4 0";
+            var successfulTestInputSplit = successfulTestStr.Split('\n');
+            int[][] successfulTest = new int[6][];
+            for (int i = 0; i < 6; i++) {
+                successfulTest[i] = Array.ConvertAll(successfulTestInputSplit[i].Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            // Act
+            var twoDArrayDS = new TwoDArrayDS();
+            var successfulTestResult = twoDArrayDS.GetLargestHourGlassValue(successfulTest);
+
+            // Assert
+            Assert.AreEqual(expectedSuccessfulTest, successfulTestResult);
+        }
+
+        [TestMethod()]
+        public void GetLargestHourGlassValueTest_Success2()
+        {
+            // Arrange
+            var expectedSuccessfulTest = 13;
+            var successfulTestStr = "1 1 1 0 0 0\n0 1 0 0 0 0\n1 1 1 0 0 0\n0 9 2 -4 -4 0\n0 0 0 -2 0 0\n0 0 -1 -2 -4 0";
+            var successfulTestInputSplit = successfulTestStr.Split('\n');
+            int[][] successfulTest = new int[6][];
+            for (int i = 0; i < 6; i++)
+            {
+                successfulTest[i] = Array.ConvertAll(successfulTestInputSplit[i].Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            // Act
+            var twoDArrayDS = new TwoDArrayDS();
+            var successfulTestResult = twoDArrayDS.GetLargestHourGlassValue(successfulTest);
+
+            // Assert
+            Assert.AreEqual(expectedSuccessfulTest, successfulTestResult);
+        }
+
+        [TestMethod()]
+        public void GetLargestHourGlassValueTest_Success3()
+        {
+            // Arrange
+            var expectedSuccessfulTest = 28;
+            var successfulTestStr = "-9 -9 -9 1 1 1\n0 -9 0 4 3 2\n-9 -9 -9 1 2 3\n0 0 8 6 6 0\n0 0 0 -2 0 0\n0 0 1 2 4 0";
+            var successfulTestInputSplit = successfulTestStr.Split('\n');
+            int[][] successfulTest = new int[6][];
+            for (int i = 0; i < 6; i++)
+            {
+                successfulTest[i] = Array.ConvertAll(successfulTestInputSplit[i].Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            // Act
+            var twoDArrayDS = new TwoDArrayDS();
+            var successfulTestResult = twoDArrayDS.GetLargestHourGlassValue(successfulTest);
+
+            // Assert
+            Assert.AreEqual(expectedSuccessfulTest, successfulTestResult);
+        }
+    }
+}
