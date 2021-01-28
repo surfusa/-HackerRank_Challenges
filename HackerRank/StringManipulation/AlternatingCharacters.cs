1cdeef980338e6e3375690f2bf114cfedc52f46e
James Phillips <surfusa@users.noreply.github.com>
2021-01-27 19:55:24 -0700

    Upload solution and test of Alternating Characters

---
+++ HackerRank/StringManipulation/AlternatingCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringManipulation
+{
+    public class AlternatingCharacters
+    {
+        public int alternatingChar (string s)
+        {
+            int counter = 0;
+            for (int i = 0; i < (s.Length - 1); i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
